15a698b2ece4ddfbcdfc14833faa7a21d39c026b
Guilherme Feitosa <guilherme_fcm@hotmail.com>
2022-11-21 19:47:57 -0300

    Create Spring Project (#5)

---
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/SistemaLocadoraVeiculoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.sistemalocadoraveiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaLocadoraVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaLocadoraVeiculoApplication.class, args);
+	}
+
+}
