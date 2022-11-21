ce7e7dd8704484a0e0738c04ff2cd2e5e0ec1b0e
Erick-Macedo-Costa <erickpart28@gmail.com>
2022-11-21 19:52:15 -0300

    criando projeto

---
+++ sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/SistemaHotelariaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.sistemahotelaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHotelariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHotelariaApplication.class, args);
+	}
+
+}
---
+++ sistema-hotelaria/src/test/java/br/edu/ifto/sistemahotelaria/SistemaHotelariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.sistemahotelaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHotelariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
