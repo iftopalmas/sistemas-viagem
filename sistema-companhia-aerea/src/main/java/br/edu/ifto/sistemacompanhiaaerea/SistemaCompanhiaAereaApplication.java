5a7ee2b4c7423336d6a4a0f3b1545f04aa69febd
Huan Carlos Cantuaria Vilarins <63238005+huan-carlos@users.noreply.github.com>
2022-11-21 20:19:17 -0300

    Cria estrutura do projeto sistema-companhia-aerea (#1)

---
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/SistemaCompanhiaAereaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.sistemacompanhiaaerea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCompanhiaAereaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCompanhiaAereaApplication.class, args);
+	}
+
+}
---
+++ sistema-companhia-aerea/src/test/java/br/edu/ifto/sistemacompanhiaaerea/SistemaCompanhiaAereaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.sistemacompanhiaaerea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCompanhiaAereaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
