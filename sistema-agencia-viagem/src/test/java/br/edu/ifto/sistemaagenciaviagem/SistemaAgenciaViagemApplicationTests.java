25c7b936241cbf3099267ae399d43a9681d63b01
Victor Nunes <nunessvictorr@gmail.com>
2022-11-21 19:50:47 -0300

    feat(agencia-viagem): comit inicial do projeto (#6)
    Refs: #4

---
+++ sistema-agencia-viagem/src/test/java/br/edu/ifto/sistemaagenciaviagem/SistemaAgenciaViagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifto.sistemaagenciaviagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAgenciaViagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
