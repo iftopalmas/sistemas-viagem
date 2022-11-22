bbc8d42949f84ba9231f448bc1a8d5dade99a34a
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 10:44:22 -0300

    Adicionada a notação @NotBlank, que, ao validar a entidade, verifica se a string cpf não está vazia ("").

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -14,6 +16,7 @@ public class Cliente {
     private Long id;
 
     @NotNull
+    @NotBlank
     @Column(unique = true)
     private String cpf;
 }
