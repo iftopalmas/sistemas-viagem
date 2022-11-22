0ed429601c4da90aef3b53b576c09f17b7097aa0
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 15:38:11 -0300

    Adicionadas notações @Id e @GeneratedValue ao atributo id da entidade Cidade, de forma que o atributo seja identificado pelo JPA como campo PrimaryKey e do tipo auto-increment.

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Cidade.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Cidade.java
@@ -1,8 +1,13 @@
 package br.edu.ifto.sistemalocadoraveiculo.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Cidade{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 }
