87d682e3fdc24dbcfda0c1bb2e4e8d67009761ae
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 10:41:59 -0300

    Adicionada notação de validação que verifica se o campo é Nulo ao validar a entidade.

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Cliente {
@@ -12,6 +13,7 @@ public class Cliente {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
     @Column(unique = true)
     private String cpf;
 }
