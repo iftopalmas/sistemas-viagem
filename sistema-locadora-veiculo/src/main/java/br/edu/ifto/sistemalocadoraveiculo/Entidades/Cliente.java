fbdc435e86861e0327c1ca6a96df7f65a5e32d01
bernard <bernard.carvalho@ifto.edu.br>
2022-11-22 09:44:21 -0300

    Adicionada notação @GeneratedValue, que informa ao spring que o valor será gerado automaticamente, com base na estatégia de tipo "AUTO"

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
@@ -1,11 +1,14 @@
 package br.edu.ifto.sistemalocadoraveiculo.Entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Cliente {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
 }
