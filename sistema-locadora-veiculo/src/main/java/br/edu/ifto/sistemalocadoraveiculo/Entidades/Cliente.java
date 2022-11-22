45c783c860cf8bcb69ec435792c6b01c403defe5
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 11:05:07 -0300

    Adicionada notação @Email de forma a verificar se o e-mail informado é valido. Ex: teste.com não é tido com valido Ex2: teste@gmail.com é tido como valido

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -36,6 +37,7 @@ public class Cliente {
     @NotBlank
     private String telefone;
 
+    @Email
     @NotNull
     @NotBlank
     @Column(unique = true)
