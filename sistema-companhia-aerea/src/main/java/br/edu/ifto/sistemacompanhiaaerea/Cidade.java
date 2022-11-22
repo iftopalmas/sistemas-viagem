31e0305b78261ec0fb9f0268ae62e0b9d9f6aa0d
Erick Macedo Costa <112912898+Erick-Macedo-Costa@users.noreply.github.com>
2022-11-21 21:53:41 -0300

    Cria classes Estado e Cidade para o sistema aéreo e veículos (#13, #16) (PR #21)

---
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/Cidade.java
@@ -0,0 +1,40 @@
+package br.edu.ifto.sistemacompanhiaaerea;
+
+import javax.persistence.*;
+
+@Entity
+public class Cidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @ManyToOne
+    private Estado estado;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+}
