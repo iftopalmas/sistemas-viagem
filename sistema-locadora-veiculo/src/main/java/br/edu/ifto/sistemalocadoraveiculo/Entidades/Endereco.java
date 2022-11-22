b4f6ea4042b4aecbfa722c1f682160db19f759f3
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 11:15:15 -0300

    Adicionada notação @GeneratedValue para informar que o campo será do tipo autoIncrement

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Endereco.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Endereco.java
@@ -1,9 +1,12 @@
 package br.edu.ifto.sistemalocadoraveiculo.Entidades;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Endereco {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String logradouro;
