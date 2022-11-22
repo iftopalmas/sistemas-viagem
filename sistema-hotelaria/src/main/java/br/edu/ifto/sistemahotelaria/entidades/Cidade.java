8bd5d41bd0f9ab551b626ad6986ee174fff0ea11
Guilherme-FCM <guilherme_fcm@hotmail.com>
2022-11-21 21:14:56 -0300

    Add GeneratedValue annotation on id column

--- sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/entidades/Cidade.java
+++ sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/entidades/Cidade.java
@@ -1,11 +1,14 @@
 package br.edu.ifto.sistemahotelaria.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Cidade {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private Long estado_id;
--- sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/entidades/Estado.java
+++ sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/entidades/Estado.java
@@ -1,12 +1,15 @@
 package br.edu.ifto.sistemahotelaria.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Estado {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String uf;
