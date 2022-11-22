0a03b078125c80d048caa08cef4cecc684571ac7
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 12:12:35 -0300

    Adicionada notação Entity a classe endereço de forma que o spring irá tratar a classe endereço como entidade para fins de persistencia de dados e injeção de dependencia. (JPA)

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Endereco.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Endereco.java
@@ -1,11 +1,13 @@
 package br.edu.ifto.sistemalocadoraveiculo.Entidades;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Entity
 public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
