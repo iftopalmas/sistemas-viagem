a4ff8540be6330238550bfbe60bb2584891a9eaa
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 15:44:15 -0300

    criado atributo Estado que referenciará o estado de determinada cidade

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Cidade.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Cidade.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -16,4 +17,6 @@ public class Cidade{
     @NotNull
     @NotBlank
     private String nome;
+
+    private Estado estado;
 }
