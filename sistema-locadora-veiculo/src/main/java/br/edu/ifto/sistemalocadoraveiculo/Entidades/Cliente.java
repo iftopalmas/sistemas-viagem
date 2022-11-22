e08703ad02ed58a77784259e3aa7ff745accf75a
bernard <bernard.carvalho@ifto.edu.br>
2022-11-22 09:42:37 -0300

    Adicionada notação @Id que informa ao jpa que o campo em questão trata-se de uma PK da entidade

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Cliente.java
@@ -1,10 +1,11 @@
 package br.edu.ifto.sistemalocadoraveiculo.Entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Cliente {
-    
+    @Id
     private Long id;
     
 }
