a4f946bc7c45d1163cd838ed287fd320a50bb797
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 15:50:03 -0300

    Adicionado campo id a entidade Estado para que o JPA seja capaz de criar e manipular o banco com o campo id da entidade sendo o primary key do tipo auto increment.

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Estado.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Estado.java
@@ -1,5 +1,11 @@
 package br.edu.ifto.sistemalocadoraveiculo.entidades;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Estado {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
