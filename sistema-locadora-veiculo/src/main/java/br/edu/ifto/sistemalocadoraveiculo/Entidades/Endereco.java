592e3ed9c7d40c6783ecf8b0c7679fff3e1ce8d1
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 11:19:43 -0300

    Adicionada notação @NotNull ao campo logradouro da classe endereco, de forma que Enderecos que não possuam um logradouro instanciado não serão validados

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Endereco.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Endereco.java
@@ -3,11 +3,13 @@ package br.edu.ifto.sistemalocadoraveiculo.Entidades;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
     private String logradouro;
 }
