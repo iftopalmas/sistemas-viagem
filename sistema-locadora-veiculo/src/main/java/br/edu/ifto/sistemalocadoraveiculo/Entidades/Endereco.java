9ecde902e22955103ef4e2c840dd42dd01057934
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 11:14:21 -0300

    Adicionada notação para que a injeção de dependencia identifique o campo como id da tabela da entidade

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Endereco.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/Entidades/Endereco.java
@@ -1,7 +1,9 @@
 package br.edu.ifto.sistemalocadoraveiculo.Entidades;
 
+import javax.persistence.Id;
+
 public class Endereco {
-    
+    @Id
     private Long id;
 
     private String logradouro;
