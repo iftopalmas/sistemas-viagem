240115df4392851947e2ed767d12dc57e42b717f
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 16:22:13 -0300

    Adicionada notação que faz com que o JPA entenda o relacionamento 1:N existente entre as entidades Estado e Cidade, terminando assim o relacionamento bi-direcional

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Estado.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Estado.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
@@ -23,5 +24,6 @@ public class Estado {
     @NotBlank
     private String uf;
 
+    @OneToMany(mappedBy = "estado")
     private List<Cidade> cidades;
 }
