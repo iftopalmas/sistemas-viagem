0b9781b905aab8cf434941ba9721f9e31617005b
Erick-Macedo-Costa <erickpart28@gmail.com>
2022-11-21 21:37:27 -0300

    criando atributos

---
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/Estado.java
@@ -0,0 +1,40 @@
+package br.edu.ifto.sistemacompanhiaaerea;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Estado {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private  String uf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
