6ffe7a32bae38ec22a68137864452c7aa0c7bde5
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-22 16:14:49 -0300

    Adicionada a notação Entity a classe estado para que o JPA entenda que a classe será persistida no banco

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Estado.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Estado.java
@@ -1,9 +1,11 @@
 package br.edu.ifto.sistemalocadoraveiculo.entidades;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Estado {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
