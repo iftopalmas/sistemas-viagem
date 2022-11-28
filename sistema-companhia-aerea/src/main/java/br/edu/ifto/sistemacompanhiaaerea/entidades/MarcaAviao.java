5be64dc4bd7646e5e56cf7702b007cda459a968f
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-11-28 01:03:00 -0300

    Adicionando os metodos gets e setes

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/entidades/MarcaAviao.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/entidades/MarcaAviao.java
@@ -9,5 +9,21 @@ public class MarcaAviao {
     private Long id;
 
     @Column(unique = true)
-    private String descricao
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
