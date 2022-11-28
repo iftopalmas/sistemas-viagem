8d5dcf190f35b01c26a6170c345ee09d5033bab8
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-11-28 01:10:08 -0300

    Adicionando o atributo marca com anotação @OneToOne

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/entidades/Aviao.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/entidades/Aviao.java
@@ -1,15 +1,18 @@
 package br.edu.ifto.sistemacompanhiaaerea.entidades;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Aviao {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    private MarcaAviao marca;
+
+
+
 
 }
