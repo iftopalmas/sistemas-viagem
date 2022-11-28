ea8249d802c81cd1b284db69ed4e4f432e759ad7
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-11-28 00:53:35 -0300

    Adicionadando o atributo id da classe com as anotações @id e @GeneratedValue

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/entidades/Aviao.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/entidades/Aviao.java
@@ -2,8 +2,14 @@ package br.edu.ifto.sistemacompanhiaaerea.entidades;
 
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Aviao {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
 }
