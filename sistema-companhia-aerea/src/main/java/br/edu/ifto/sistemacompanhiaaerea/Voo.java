0bc47269c8fe7f03fd0d3ac28b44707d7db1dd71
BernardCarvalho <bernard.carvalho@estudante.ifto.edu.br>
2022-11-29 08:36:40 -0300

    Adicionados atributos da classe Voo

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/Voo.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/Voo.java
@@ -1,5 +1,39 @@
 package br.edu.ifto.sistemacompanhiaaerea;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import br.edu.ifto.sistemacompanhiaaerea.entidades.Aviao;
+
 public class Voo {
     
+    @NotBlank
+    @Column(unique = true)
+    private String numero;
+    
+    @NotNull
+    private LocalDateTime dataHoraPartidaEsperada;
+    
+    @NotNull
+    private LocalDateTime dataHoraChegadaEsperada;
+
+    @NotNull
+
+    private Aviao aviao;
+    
+    // @NotNull
+    // private Piloto piloto;
+    // TODO: Depende de #51
+
+    // @NotNull
+    // private Aeroporto aeroportoOrigem;
+
+    // @NotNull
+    // private Aeroporto aeroportoDestino;
+    // TODO: Depende de #52
+    
+
 }
