3243df5b7ee3e8ad491b93466a2ebb76234950fb
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-11-28 21:07:32 -0300

    Adicionando os metódos Getters e Setters da classe

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/entidades/Aviao.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/entidades/Aviao.java
@@ -28,4 +28,52 @@ public class Aviao {
     @NotNull
     @Min(1)
     private int totalAssentos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MarcaAviao getMarca() {
+        return marca;
+    }
+
+    public void setMarca(MarcaAviao marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPrefixo() {
+        return prefixo;
+    }
+
+    public void setPrefixo(String prefixo) {
+        this.prefixo = prefixo;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public int getTotalAssentos() {
+        return totalAssentos;
+    }
+
+    public void setTotalAssentos(int totalAssentos) {
+        this.totalAssentos = totalAssentos;
+    }
 }
