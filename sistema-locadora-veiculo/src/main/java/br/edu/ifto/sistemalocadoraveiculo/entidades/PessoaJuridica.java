fff091fe6ab2af6e47396ee63917a44416e9edd2
BernardCarvalho <99513042+BernardCarvalho@users.noreply.github.com>
2022-11-29 13:45:17 -0300

    Classe atualizada para utilizar Lombok.
    Os metodos Setters e Getters são criados automaticamente pelas notações `@Getter` e `@Setter`

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/PessoaJuridica.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/PessoaJuridica.java
@@ -5,32 +5,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
 public class PessoaJuridica {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private long id;
+    
     private String nome;
+    
     private int cnpj;
 
-
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public int getCnpj() {
-        return cnpj;
-    }
-    public void setCnpj(int cnpj) {
-        this.cnpj = cnpj;
-    }
 }
