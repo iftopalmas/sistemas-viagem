c40a84007c727fcbeca7c7993d59ae40b30070f7
addahregina <addahregina@gmail.com>
2022-11-29 11:18:30 -0300

    #59

---
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Reserva.java
@@ -0,0 +1,33 @@
+package br.edu.ifto.sistemalocadoraveiculo.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+
+
+public class Reserva {
+    @NotNull
+    private LocalDateTime dataHoraCadastro;
+
+    @NotNull
+    private LocalDateTime dataHoraRetiradaEsperada;
+
+    @NotNull
+    private LocalDateTime dataHoraDevolucaoEsperada;
+
+    @NotNull
+    private Cliente cliente;
+
+    @NotNull
+    private  Locadora locadoraRetirada;
+
+    @NotNull
+    private  Locadora locadoraDevolucao;
+
+}
+
