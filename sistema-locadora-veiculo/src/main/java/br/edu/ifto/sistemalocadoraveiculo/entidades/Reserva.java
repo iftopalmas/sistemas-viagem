14915b42dbe3f108825837ecbdb804b4eebfb4b7
Guilherme Alexandre Pereira Lima <113989944+HadisBra@users.noreply.github.com>
2022-12-01 11:08:21 -0300

    Update Reserva.java
    acrescentei alguns pacotes
    acrescentei um  ID.

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Reserva.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Reserva.java
@@ -2,15 +2,24 @@ package br.edu.ifto.sistemalocadoraveiculo.entidades;
 
 import lombok.Getter;
 import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 
 @Getter
 @Setter
-
+@Entity
 
 public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  long id;
+    
     @NotNull
     private LocalDateTime dataHoraCadastro;
 
