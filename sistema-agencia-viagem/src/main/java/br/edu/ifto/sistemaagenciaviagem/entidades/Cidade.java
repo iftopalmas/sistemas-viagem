559b14c772ce5e67516dfafbb79de6f19ca94733
flavio <flaviomouracar7800@gmail.com>
2022-12-03 20:19:39 -0300

    Adicionado na classe Cidade do projeto sistema-agencia-viagem o atributo 'id' com as anotações Id e GeneratedValue.

--- sistema-agencia-viagem/src/main/java/br/edu/ifto/sistemaagenciaviagem/entidades/Cidade.java
+++ sistema-agencia-viagem/src/main/java/br/edu/ifto/sistemaagenciaviagem/entidades/Cidade.java
@@ -3,9 +3,15 @@ package br.edu.ifto.sistemaagenciaviagem.entidades;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 public class Cidade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
 }
