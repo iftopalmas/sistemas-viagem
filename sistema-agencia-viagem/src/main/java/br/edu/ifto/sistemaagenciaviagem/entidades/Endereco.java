e4ca5a08daa75a5a3190bc8cd32f7894b32c9fe7
flavio <flaviomouracar7800@gmail.com>
2022-12-03 19:57:30 -0300

    Adicionado na classe endereço do projeto sistema-agencia-viagem o atributo 'id' com as anotações Id e GeneratedValue.

--- sistema-agencia-viagem/src/main/java/br/edu/ifto/sistemaagenciaviagem/entidades/Endereco.java
+++ sistema-agencia-viagem/src/main/java/br/edu/ifto/sistemaagenciaviagem/entidades/Endereco.java
@@ -3,9 +3,15 @@ package br.edu.ifto.sistemaagenciaviagem.entidades;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
 }
