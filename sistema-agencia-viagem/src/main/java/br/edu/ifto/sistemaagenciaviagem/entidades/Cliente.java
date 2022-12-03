2d540e40fc97e34e6049971dc1fda20694c46a5f
flavio <flaviomouracar7800@gmail.com>
2022-12-03 18:59:42 -0300

    Adicionado na classe cliente do projeto sistema-agencia-viagem o atributo 'id' com as anotações Id e GeneratedValue.

--- sistema-agencia-viagem/src/main/java/br/edu/ifto/sistemaagenciaviagem/entidades/Cliente.java
+++ sistema-agencia-viagem/src/main/java/br/edu/ifto/sistemaagenciaviagem/entidades/Cliente.java
@@ -3,9 +3,14 @@ package br.edu.ifto.sistemaagenciaviagem.entidades;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class Cliente {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
