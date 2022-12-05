399af22f1f4866669b6a16c433f8d82f61470939
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-12-05 20:58:31 -0300

    Modificando a classe Locadora, mudando as anotações dos atributos e adicionando a notação Data

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Locadora.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Locadora.java
@@ -1,20 +1,19 @@
 package br.edu.ifto.sistemalocadoraveiculo.entidades;
 
 import com.sun.istack.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+
 
 import javax.persistence.*;
 
-@Getter
-@Setter
+@Data
 @Entity
 public class Locadora extends PessoaJuridica {
-
     @NotNull
     private boolean matriz;
-    
+
     @NotNull
+    @Column(unique = true)
     private String codigoIataAeroporto;
 
 }
