3351a3b4c23488e08864833817fc29553e1bb3f3
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-12-05 21:01:51 -0300

    Modificando a classe PessoaJuridica, mudando as anotações dos atributos e adicionando algumas anotações como CNPJ, COLUMN(unique=true) e Data

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/PessoaJuridica.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/PessoaJuridica.java
@@ -1,24 +1,27 @@
 package br.edu.ifto.sistemalocadoraveiculo.entidades;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CNPJ;
 
-@Getter
-@Setter
+@Data
 @Entity
 public class PessoaJuridica {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    
-    private String nome;
-    
-    private int cnpj;
+
+    @NotBlank
+    private String razaoSocial;
+
+    @CNPJ
+    @NotBlank
+    @Column(unique = true)
+    private String cnpj;
 
 }
