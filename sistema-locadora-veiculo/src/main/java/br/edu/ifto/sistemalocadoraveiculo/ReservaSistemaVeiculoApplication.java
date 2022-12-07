d687ed06b46696c23a8c3ca306f0cff376bcb61a
Manoel Campos <manoelcampos@gmail.com>
2022-12-06 22:29:24 -0300

    Delete ReservaSistemaVeiculoApplication.java

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/ReservaSistemaVeiculoApplication.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifto.sistemalocadoraveiculo;
-
-import org.springframework.boot.SpringApplication;
-
-public class ReservaSistemaVeiculoApplication {
-    public static void main(String[] args) { SpringApplication.run(ReservaSistemaVeiculoApplication.class, args); }
-}
