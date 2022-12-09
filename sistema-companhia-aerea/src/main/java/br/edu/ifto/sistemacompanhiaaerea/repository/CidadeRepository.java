9a52e45cc8756898388a562a94fa42927e590d24
flavio <flaviomouracar7800@gmail.com>
2022-12-09 11:01:11 -0300

    Adicionado no arquivo interface CidadeRepository um atributo findCidadeByEstado_uf no projeto sistema-companhia-aerea.

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/CidadeRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/CidadeRepository.java
@@ -1,5 +1,6 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
 
+import br.edu.ifto.sistemacompanhiaaerea.model.Estado;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -11,5 +12,6 @@ import java.util.List;
 public interface CidadeRepository extends PagingAndSortingRepository<Cidade, Long> {
     Cidade findCidadeById(@Param("id") Long id);
     Cidade findCidadeByNome(@Param("nome") String nome);
+    List<Cidade> findCidadeByEstado_Uf(@Param("uf") String uf);
 
 }
