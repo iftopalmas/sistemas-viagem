1a6d63d25ef0f1d1362985939d3161fafb341d33
flavio <flaviomouracar7800@gmail.com>
2022-12-09 09:45:16 -0300

    Adicionada a extensão PagingAndSortingRepository no arquivo de interface CidadeRepository do sistema-companhia-aerea

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/CidadeRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/CidadeRepository.java
@@ -1,7 +1,9 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import br.edu.ifto.sistemacompanhiaaerea.model.Cidade;
 
 @RepositoryRestResource(collectionResourceRel = "cidades", path = "cidades")
-public interface CidadeRepository {
+public interface CidadeRepository extends PagingAndSortingRepository<Cidade, Long> {
 }
