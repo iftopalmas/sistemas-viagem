f868ca770fe2d8612c39f171db6fc6e74b00cc7a
flavio <flaviomouracar7800@gmail.com>
2022-12-09 09:57:58 -0300

    Adicionado no arquivo interface CidadeRepository um atributo findCidadeById no projeto sistema-companhia-aerea.

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/CidadeRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/CidadeRepository.java
@@ -1,9 +1,14 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import br.edu.ifto.sistemacompanhiaaerea.model.Cidade;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "cidades", path = "cidades")
 public interface CidadeRepository extends PagingAndSortingRepository<Cidade, Long> {
+    Cidade findCidadeById(@Param("id") Long id);
+
 }
