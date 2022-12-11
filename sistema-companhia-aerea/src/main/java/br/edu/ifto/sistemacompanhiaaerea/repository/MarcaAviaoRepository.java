2732fd22934252423ec137a9a1fd3148d8af3fd7
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-12-11 16:27:20 -0300

    Adicionando a anotação RepositoryRestResource no arquivo MarcaAviaoRepository do diretório Repository do projeto sistema-aereo

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/MarcaAviaoRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/MarcaAviaoRepository.java
@@ -1,4 +1,7 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "marcasAvioes", path = "marcasAvioes")
 public interface MarcaAviaoRepository {
 }
