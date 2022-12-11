19372be13d6f0c493f85314abcc504488bfe971f
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-12-11 16:39:54 -0300

    Adicionando o método findMarcaAviaoById no arquivo MarcaAviaoRepository do diretório Repository do projeto sistema-aereo

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/MarcaAviaoRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/MarcaAviaoRepository.java
@@ -2,8 +2,11 @@ package br.edu.ifto.sistemacompanhiaaerea.repository;
 
 import br.edu.ifto.sistemacompanhiaaerea.model.MarcaAviao;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "marcasAvioes", path = "marcasAvioes")
 public interface MarcaAviaoRepository extends PagingAndSortingRepository<MarcaAviao, Long> {
+    MarcaAviao findMarcaAviaoById(@Param("id") Long id);
+
 }
