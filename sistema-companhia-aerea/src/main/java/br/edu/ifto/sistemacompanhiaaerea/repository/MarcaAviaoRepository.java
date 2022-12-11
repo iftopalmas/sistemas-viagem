8b4382810c856d9159ba324cc13a31f2215b0a56
viiniciusmoura <viniciusmirandamoura5@gmail.com>
2022-12-11 16:29:45 -0300

    Extendendo a classe PagingAndSortingRepository para o arquivo MarcaAviaoRepository do diretório Repository do projeto sistema-aereo

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/MarcaAviaoRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/MarcaAviaoRepository.java
@@ -1,7 +1,9 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
 
+import br.edu.ifto.sistemacompanhiaaerea.model.MarcaAviao;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "marcasAvioes", path = "marcasAvioes")
-public interface MarcaAviaoRepository {
+public interface MarcaAviaoRepository extends PagingAndSortingRepository<MarcaAviao, Long> {
 }
