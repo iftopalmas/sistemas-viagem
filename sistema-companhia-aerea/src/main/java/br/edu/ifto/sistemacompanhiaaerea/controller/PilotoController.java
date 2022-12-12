ce95215485f928c13dbc16817939d2ff299a5282
Victor Nunes <nunessvictorr@gmail.com>
2022-12-12 14:42:46 -0300

    feat(companhia-aerea): add listagem de pilotos
    Refs: #81

---
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/controller/PilotoController.java
@@ -0,0 +1,23 @@
+package br.edu.ifto.sistemacompanhiaaerea.controller;
+
+import br.edu.ifto.sistemacompanhiaaerea.model.Piloto;
+import br.edu.ifto.sistemacompanhiaaerea.repository.PilotoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+public class PilotoController {
+    private final PilotoRepository repository;
+
+    public PilotoController(PilotoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/pilotos")
+    public List<Piloto> findAll() {
+        return repository.findAll();
+    }
+}
--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/PilotoRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/PilotoRepository.java
@@ -2,8 +2,7 @@ package br.edu.ifto.sistemacompanhiaaerea.repository;
 
 import br.edu.ifto.sistemacompanhiaaerea.model.Piloto;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "pilotos", path = "pilotos")
 public interface PilotoRepository extends JpaRepository<Piloto, Long> {
+
 }
