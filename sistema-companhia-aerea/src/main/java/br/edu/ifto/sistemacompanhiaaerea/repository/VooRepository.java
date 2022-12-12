c8dfe1756e6c6199517fa83be6922b95cdc94fed
Manoel Campos <manoelcampos@gmail.com>
2022-12-12 10:30:37 -0300

    Faltou indicar qual verbo HTTP será usado para acessar os endpoints definidos pelos métodos introduzidos.
    Como é consulta, é GetMapping (verbo HTTP GET)
    
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/VooRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/VooRepository.java
@@ -6,6 +6,7 @@ import br.edu.ifto.sistemacompanhiaaerea.model.Aeroporto;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,7 @@ import java.util.Optional;
 
 @RepositoryRestResource (collectionResourceRel ="voos",path = "voos")
 public interface VooRepository extends PagingAndSortingRepository <Voo , Long> {
+    @GetMapping("/numero/{numero}")
     Optional <Voo> findByNumero(@Param("numero") String numero);
 
     List <Aeroporto> findByAeroportoOrigem (@Param("aeroportoOrigem")Long aeroportoOrigem);
