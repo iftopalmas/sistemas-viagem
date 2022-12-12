30d179088099003a1c7bc73e7afe99dfa06d3445
Victor Nunes <nunessvictorr@gmail.com>
2022-12-12 14:43:33 -0300

    feat(companhia-aerea): add get piloto por id
    Refs: #81

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/controller/PilotoController.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/controller/PilotoController.java
@@ -3,6 +3,7 @@ package br.edu.ifto.sistemacompanhiaaerea.controller;
 import br.edu.ifto.sistemacompanhiaaerea.model.Piloto;
 import br.edu.ifto.sistemacompanhiaaerea.repository.PilotoRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,5 +20,10 @@ public class PilotoController {
     @GetMapping("/pilotos")
     public List<Piloto> findAll() {
         return repository.findAll();
+    }
+
+    @GetMapping("/pilotos/{id}")
+    public Piloto findById(@PathVariable Long id) {
+        return repository.findById(id).orElseThrow();
     }
 }
