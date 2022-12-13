a6616962c389ac4da2627f91706ca8fdde500dc8
Danilo <danilo.custodio@estudante.ifto.edu.br>
2022-12-13 10:52:05 -0300

    Inclui classe Resource

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/repositories/CategoriaVeiculoRepository.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/repositories/CategoriaVeiculoRepository.java
@@ -4,10 +4,7 @@ import br.edu.ifto.sistemalocadoraveiculo.entidades.CategoriaVeiculo;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 @RepositoryRestResource(collectionResourceRel = "categoriaVeiculos", path = "categoriaVeiculos")
 public interface CategoriaVeiculoRepository extends PagingAndSortingRepository<CategoriaVeiculo, Long> {
-    List<CategoriaVeiculo> findById(@PathVariable String id);
-    List<CategoriaVeiculo> findByDescricao(@PathVariable String descricao);
+    CategoriaVeiculo findByDescricao(@PathVariable String descricao);
 }
