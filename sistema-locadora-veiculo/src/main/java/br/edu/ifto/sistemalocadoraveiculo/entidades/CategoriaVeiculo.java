94f18ff13c90049ebdd2b1bbee9dc5b5e3ddff3e
Danilo <danilo.custodio@estudante.ifto.edu.br>
2022-12-13 10:16:50 -0300

    Alterações feitas

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/CategoriaVeiculo.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/CategoriaVeiculo.java
@@ -2,12 +2,9 @@ package br.edu.ifto.sistemalocadoraveiculo.entidades;
 
 import lombok.Getter;
 import lombok.Setter;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -17,9 +14,9 @@ public class CategoriaVeiculo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
-    @NotBlank
+    @NotNull
     @Column(unique = true)
     private String descricao;
-    @NotBlank
+    @NotNull
     private String detalhes;
 }
