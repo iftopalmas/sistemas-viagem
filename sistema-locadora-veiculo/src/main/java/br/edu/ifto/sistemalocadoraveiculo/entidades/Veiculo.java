8204f00a69d01e118cb4ecad0e0c2f320704890d
Danilo <danilo.custodio@estudante.ifto.edu.br>
2022-12-13 10:33:23 -0300

    Coloca de volta anotacoes Getter e Setter

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Veiculo.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/Veiculo.java
@@ -1,9 +1,14 @@
 package br.edu.ifto.sistemalocadoraveiculo.entidades;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Getter
+@Setter
 @Entity
 public class Veiculo {
     @Id
