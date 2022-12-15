686586761e71b4be2e4592f8fd6f37a31b47fc5b
Manoel Campos <manoelcampos@gmail.com>
2022-12-15 16:42:29 -0300

    Update CategoriaVeiculo.java

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/CategoriaVeiculo.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/CategoriaVeiculo.java
@@ -23,12 +23,4 @@ public class CategoriaVeiculo {
     
     @NotNull
     private String detalhes;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
