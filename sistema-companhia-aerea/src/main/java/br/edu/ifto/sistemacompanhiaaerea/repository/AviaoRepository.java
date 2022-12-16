2573761ed3aa2dc549082c9dba85ec9fee629a63
Erick-Macedo-Costa <erickpart28@gmail.com>
2022-12-15 23:48:52 -0300

    adicionado a anotaçao RepositoryRestResource no AviaoRepository

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/AviaoRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/AviaoRepository.java
@@ -1,4 +1,7 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
 
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "Aviao", path = "Avioes")
 public interface AviaoRepository {
 }
