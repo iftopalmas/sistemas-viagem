8aad48dd5f4d51697453b75dd9c4df9f8b74524f
Erick-Macedo-Costa <erickpart28@gmail.com>
2022-12-15 23:50:02 -0300

    adicionado a extends PagingAndSortingRepository no AviaoRepository

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/AviaoRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/AviaoRepository.java
@@ -1,7 +1,9 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
 
+import br.edu.ifto.sistemacompanhiaaerea.model.Aviao;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "Aviao", path = "Avioes")
-public interface AviaoRepository {
+public interface AviaoRepository extends PagingAndSortingRepository<Aviao, Long> {
 }
