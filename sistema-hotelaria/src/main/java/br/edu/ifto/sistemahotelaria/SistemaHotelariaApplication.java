935b9eb2336ea692b9190038b386d018b1a063ce
Eliel Oliver <eliel.viana@grupomateus.com>
2023-05-29 20:08:55 -0300

    Adiciona comentario

--- sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/SistemaHotelariaApplication.java
+++ sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/SistemaHotelariaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SistemaHotelariaApplication {
 
 	public static void main(String[] args) {
+		//teste
 		SpringApplication.run(SistemaHotelariaApplication.class, args);
 	}
 
