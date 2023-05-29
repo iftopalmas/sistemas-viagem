96bedf075fb308b455729e3ac35aaabdc455f7c2
Andrew Magalhães <81531887+okalil@users.noreply.github.com>
2023-05-29 20:52:38 -0300

    Adiciona entidade Endereco

---
+++ sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/entidades/Endereco.java
@@ -0,0 +1,27 @@
+package br.edu.ifto.sistemahotelaria.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    private String logradouro;
+
+    @NotNull
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Cidade cidade;
+
+    private String bairro;
+
+}
