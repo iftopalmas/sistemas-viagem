13baa1c9a840d9d22f14bdf2ca820fda4cff6348
Andrew Magalhães <81531887+okalil@users.noreply.github.com>
2023-05-29 20:48:17 -0300

    Adiciona atributos classe Cliente

--- sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/entidades/Cliente.java
+++ sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/entidades/Cliente.java
@@ -2,12 +2,32 @@ package br.edu.ifto.sistemahotelaria.entidades;
 
 import lombok.Getter;
 import lombok.Setter;
-import javax.persistence.Entity;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+
 
 @Entity
 @Getter
 @Setter
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
     private String nome;
-    private long id, cpf, fone;
+
+    @CPF
+    @Column(unique = true)
+    private String cpf;
+
+    private LocalDate dataNascimento;
+
+    @NotBlank
+    private String celular;
+
+    private boolean fumante;
 }
