c35399612d83d9f84b32ae515709d10d2247e319
Eliel Oliver <eliel.viana@grupomateus.com>
2023-05-29 21:21:07 -0300

    Remove comentario adicionado para fins de teste em sala de aula.

--- sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/SistemaHotelariaApplication.java
+++ sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/SistemaHotelariaApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SistemaHotelariaApplication {
 
-	public static void main(String[] args) {
-		//teste
-		SpringApplication.run(SistemaHotelariaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaHotelariaApplication.class, args);
+    }
 
 }
