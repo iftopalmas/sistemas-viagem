b06526f653850ce50e0798d79783aaf5c71e28d6
Eliel Oliver <eliel.viana@grupomateus.com>
2023-05-29 22:01:19 -0300

    Ajusta anotação fazendo troca de @Param para @PathVariable.

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/VooRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/VooRepository.java
@@ -1,10 +1,12 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
 
 import br.edu.ifto.sistemacompanhiaaerea.model.Voo;
+import br.edu.ifto.sistemacompanhiaaerea.model.CompanhiaAerea;
+import br.edu.ifto.sistemacompanhiaaerea.model.Aeroporto;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,14 +15,14 @@ import java.util.Optional;
 @RepositoryRestResource (collectionResourceRel ="voos", path = "voos")
 public interface VooRepository extends PagingAndSortingRepository <Voo , Long> {
     @GetMapping("/numero/{numero}")
-    Optional <Voo> findByNumero(@Param("numero") String numero);
+    Optional <Voo> findByNumero(@PathVariable String numero);
 
     @GetMapping("/origem/{aeroportoId}")
-    List <Voo> findByAeroportoOrigemId(@Param("aeroportoId") Long aeroportoId);
+    List <Voo> findByAeroportoOrigem (@PathVariable Long aeroportoId);
 
     @GetMapping("/destino/{aeroportoId}")
-    List <Voo> findByAeroportoDestinoId(@Param("aeroportooId") Long aeroportoId);
+    List <Voo> findByAeroportoDestino(@PathVariable Long aeroportoId);
 
     @GetMapping("/companhia/{companhiaId}")
-    List<Voo> findByCompanhiaAereaId(@Param("companhiaId") Long companhiaId);
+    List<Voo> findByNome(@PathVariable Long companhiaId);
 }
