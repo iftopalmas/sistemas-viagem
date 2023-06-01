f4892010d4b717aef8971577d03b51943f847550
Manoel Campos <manoelcampos@gmail.com>
2023-06-01 16:28:39 -0300

    Refatora encerramento dos servidores
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/SistemaHotelariaApplication.java
+++ sistema-hotelaria/src/main/java/br/edu/ifto/sistemahotelaria/SistemaHotelariaApplication.java
@@ -11,11 +11,11 @@ public class SistemaHotelariaApplication {
     /**
      * Inicia o servidor de aplicação.
      * @param args argumentos de linha de comando.
-     *             Passe shutdown, stop ou kill para fechar o servidor depois da inicialização
+     *             Passe shutdown, stop, close ou kill para fechar o servidor depois da inicialização
      *             (para propósitos de testes).
      */
     public static void main(String[] args) {
-        final boolean shutdown = (args.length > 0 && (args[0].equalsIgnoreCase("shutdown") || args[0].equalsIgnoreCase("stop") || args[0].equalsIgnoreCase("kill")));
+        final boolean shutdown = (args.length > 0 && ("shutdown".equalsIgnoreCase(args[0]) || "stop".equalsIgnoreCase(args[0]) | "close".equalsIgnoreCase(args[0]) || "kill".equalsIgnoreCase(args[0])));
         ctx = SpringApplication.run(SistemaHotelariaApplication.class, args);
         if(shutdown) {
             System.out.printf("%n%n# Recebida solicitação do usuário, via linha de comando, para desligar o servidor.%n");
