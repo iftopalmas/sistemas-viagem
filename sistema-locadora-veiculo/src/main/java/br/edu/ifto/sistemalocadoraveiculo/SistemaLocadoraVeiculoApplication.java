e16ff0abb5209949c48af20fee05f8cbabf831c6
Manoel Campos <manoelcampos@gmail.com>
2023-06-01 15:37:47 -0300

    Permitir rodar o servidor e finalizar imediatamente, para propósitos de teste, por meio de um argumento shutdown na linha de comando.
    Assim, no CI é possível rodar o servidor
    e parar imediatamente para verificar
    se não tá dando erro de inicialização
    (como no caso de nomes de métodos de repositórios
    spring fora do padrão).
    
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/SistemaLocadoraVeiculoApplication.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/SistemaLocadoraVeiculoApplication.java
@@ -1,13 +1,35 @@
 package br.edu.ifto.sistemalocadoraveiculo;
 
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SistemaLocadoraVeiculoApplication {
+    public static ConfigurableApplicationContext ctx;
+    private static boolean shutdown = false;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaLocadoraVeiculoApplication.class, args);
-	}
+    /**
+     * Inicia o servidor de aplicação.
+     * @param args argumentos de linha de comando.
+     *             Passe shutdown, stop ou kill para fechar o servidor depois da inicialização
+     *             (para propósitos de testes).
+     */
+    public static void main(String[] args) {
+        shutdown = (args.length > 0 && (args[0].equalsIgnoreCase("shutdown") || args[0].equalsIgnoreCase("stop") || args[0].equalsIgnoreCase("kill")));
+        ctx = SpringApplication.run(SistemaLocadoraVeiculoApplication.class, args);
+        if(shutdown) {
+            System.out.printf("%n%n# Recebida solicitação do usuário, via linha de comando, para desligar o servidor.%n");
+            new Thread(SistemaLocadoraVeiculoApplication::shutdown).start();
+        }
+    }
 
+
+    @SneakyThrows
+    public static void shutdown() {
+        Thread.sleep(2000);
+        System.out.printf("# Desligando o servidor por requisição do usuário via linha de comando.%n%n");
+        ctx.close();
+    }
 }
