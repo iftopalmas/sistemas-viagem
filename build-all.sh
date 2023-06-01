4a08ad5ce327d10cd5405d03c8c8901501ff8055
Manoel Campos <manoelcampos@gmail.com>
2023-06-01 16:29:03 -0300

    Atualiza build-all.sh para permitir compilar e inicializar servidores, para verificar erros de inicialização
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 echo "Compilando todos os projetos"
-mvn -B clean package --file sistema-agencia-viagem/pom.xml
-mvn -B clean package --file sistema-companhia-aerea/pom.xml
-mvn -B clean package --file sistema-hotelaria/pom.xml
-mvn -B clean package --file sistema-locadora-veiculo/pom.xml
+mvn -B -quiet clean package --file sistema-agencia-viagem/pom.xml
+mvn -B -quiet clean package --file sistema-companhia-aerea/pom.xml
+mvn -B -quiet clean package --file sistema-hotelaria/pom.xml
+mvn -B -quiet clean package --file sistema-locadora-veiculo/pom.xml
+
+if [[ "$1" == "shutdown" || "$1" == "stop" || "$1" == "close" || "$1" == "kill" ]]; then
+  echo "Executando servidores para verificar possíveis erros de inicialização"
+  cd sistema-agencia-viagem   && java -jar target/*.jar shutdown; cd ..
+  cd sistema-companhia-aerea  && java -jar target/*.jar shutdown; cd ..
+  cd sistema-hotelaria        && java -jar target/*.jar shutdown; cd ..
+  cd sistema-locadora-veiculo && java -jar target/*.jar shutdown
+fi
