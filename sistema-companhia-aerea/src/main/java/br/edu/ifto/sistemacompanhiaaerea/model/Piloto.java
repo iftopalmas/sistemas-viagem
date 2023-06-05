1eab1849e5d6aafdc68d5c5aca4b7a9655570a42
Manoel Campos <manoelcampos@gmail.com>
2023-06-05 19:55:35 -0300

    Atualiza Piloto docs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/model/Piloto.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/model/Piloto.java
@@ -1,18 +1,12 @@
 package br.edu.ifto.sistemacompanhiaaerea.model;
 
-import java.time.LocalDate;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -25,6 +19,9 @@ public class Piloto {
     @OneToOne(cascade = CascadeType.ALL)
     private PessoaFisica pessoaFisica;
 
+    /**
+     * Número do Brevê do piloto.
+     */
     @NotNull
     @NotBlank
     @Column(unique = true)
