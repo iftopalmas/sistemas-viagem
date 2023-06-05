d362f7574d7fcd990f822022000038b76c49441f
Eliel Oliver <eliel.viana@grupomateus.com>
2023-06-05 20:48:22 -0300

    Ajusta metodo de pesquisa do piloto e assinatura do metodo.

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/PilotoRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/PilotoRepository.java
@@ -1,13 +1,21 @@
 package br.edu.ifto.sistemacompanhiaaerea.repository;
+
 import br.edu.ifto.sistemacompanhiaaerea.model.Piloto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
+
 @RepositoryRestResource(collectionResourceRel = "pilotos", path = "pilotos")
 public interface PilotoRepository extends PagingAndSortingRepository<Piloto, Long> {
-    Optional<Piloto> findByPessoaFisicaCpf(@Param("cpf") String cpf);
-    Optional<List<Piloto>> findPilotoByAtivo(@Param("ativo") Boolean ativo);
+    @GetMapping("/cpf/{cpf}")
+    Optional<Piloto> findByPessoaFisicaCpf(@PathVariable String cpf);
+
+    @GetMapping("/ativo/{id}")
+    @Query("select p from Piloto p where p.ativo = true and p.companhiaAerea.id = :id")
+    List<Piloto> findPilotoByAtivo(@PathVariable Long id);
 }
