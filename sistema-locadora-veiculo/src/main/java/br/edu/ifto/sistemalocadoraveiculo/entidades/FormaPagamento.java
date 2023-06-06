21328a9b9a2728dc0ac862795cba3faae42539a8
Manoel Campos <manoelcampos@gmail.com>
2023-06-06 08:17:48 -0300

    Update FormaPagamento.java

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/FormaPagamento.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/entidades/FormaPagamento.java
@@ -12,7 +12,7 @@ public class FormaPagamento {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @NotBlank
     @Column(unique = true)
