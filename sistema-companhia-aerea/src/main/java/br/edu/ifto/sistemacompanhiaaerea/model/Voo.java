ff759ad5dbd6cf5b6120386acc570840aaf1269f
Manoel Campos <manoelcampos@gmail.com>
2023-06-06 10:42:21 -0300

    Altera exceções na classe Voo
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/model/Voo.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/model/Voo.java
@@ -48,7 +48,7 @@ public class Voo {
     public void setAeroportoOrigem(Aeroporto aeroportoOrigemArg) throws Exception {
         if (this.getAeroportoDestino() != null) {
             if (aeroportoOrigemArg.getCodigoIata().equals(this.getAeroportoDestino().getCodigoIata()))
-                throw new Exception("[ERRO] : Não foi possivel alterar o aeroporto de origem, pois ele já é o aeroporto de destino");
+                throw new IllegalArgumentException("Não foi possivel alterar o aeroporto de origem, pois ele já é o aeroporto de destino");
         }
         this.aeroportoOrigem = aeroportoOrigemArg;
     }
@@ -56,7 +56,7 @@ public class Voo {
     public void setAeroportoDestino(Aeroporto aeroportoDestinoArg) throws Exception {
         if (this.getAeroportoOrigem() != null) {
             if (aeroportoDestinoArg.getCodigoIata().equals(this.getAeroportoOrigem().getCodigoIata()))
-                throw new Exception("[ERRO] : Não foi possivel alterar o aeroporto de destino, pois ele já é o aeroporto de origem");
+                throw new IllegalArgumentException("Não foi possivel alterar o aeroporto de destino, pois ele já é o aeroporto de origem");
         }
         this.aeroportoDestino = aeroportoDestinoArg;
     }
