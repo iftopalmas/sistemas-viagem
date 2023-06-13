758e62ff041e625f4aeeeee9719589bd6e7478dd
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-06-13 11:49:27 -0300

    Implementando operações CRUD através de JpaRepository

--- sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/repositories/VeiculoRepository.java
+++ sistema-locadora-veiculo/src/main/java/br/edu/ifto/sistemalocadoraveiculo/repositories/VeiculoRepository.java
@@ -3,6 +3,7 @@ package br.edu.ifto.sistemalocadoraveiculo.repositories;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -10,7 +11,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import br.edu.ifto.sistemalocadoraveiculo.entidades.Veiculo;
 
 @RepositoryRestResource(collectionResourceRel = "veiculos", path = "veiculos")
-public interface VeiculoRepository extends PagingAndSortingRepository<Veiculo, Long> {
+public interface VeiculoRepository extends PagingAndSortingRepository<Veiculo, Long>, JpaRepository<Veiculo, Long> {
   Veiculo findByPlaca(@Param("placa") String placa);
   List<Veiculo> findByLocadoraId(@Param("locadoraId") long locadoraId);
   List<Veiculo> findByLocadoraIdAndCategoriaId(@Param("locadoraId") long locadoraId, @Param("categoriaId") long categoriaId);
