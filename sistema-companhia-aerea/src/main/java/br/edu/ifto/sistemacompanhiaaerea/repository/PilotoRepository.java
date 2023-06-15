e962586f47faf33a17b6aab2c46fb41fee0625e5
Charles Albert Martins dos Anjos <charles.ccomp@gmail.com>
2023-06-15 09:20:08 -0300

    Close #144 (PR #153)
    * Remove PilotoController pois o PilotoRepository já cria os endpoints REST
    * Adiciona dependência correta para spring data rest e openapi (springdoc)
    * Adiciona JavaDocs
    * Usa text blocks do JDK 15 pra não precisar concatenar strings
    
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/PilotoRepository.java
+++ sistema-companhia-aerea/src/main/java/br/edu/ifto/sistemacompanhiaaerea/repository/PilotoRepository.java
@@ -17,5 +17,18 @@ public interface PilotoRepository extends PagingAndSortingRepository<Piloto, Lon
 
     @GetMapping("/ativo/{companhiaId}")
     @Query("select p from Piloto p where p.ativo = true and p.companhiaAerea.id = :companhiaId")
-    List<Piloto> findPilotosAtivosByCompanhiaId(@PathVariable Long companhiaId);
+    List<Piloto> findPilotosAtivosByCompanhiaId(@PathVariable long companhiaId);
+
+    @GetMapping("/total-horas-voo/{pilotoId}")
+    @Query("select sum(datediff(hour, v.dataHoraPartida,v.dataHoraChegada)) from Voo v where v.piloto.id = :pilotoId")
+    long getHorasVooByPilotoId(@PathVariable long pilotoId);
+
+    /** {@return uma matriz de duas colunas} Cada linha representa um registro onde o id do piloto está na 1ª coluna
+     *  e a média está no 2ª. */
+    @GetMapping("/media-horas-voo/{companhiaAereaId}")
+    @Query("""
+           select v.piloto.id as pilotoId, avg(datediff(hour, v.dataHoraPartida,v.dataHoraChegada)) as mediaHorasVoo
+           from Voo v where v.companhiaAerea.id = :companhiaAereaId group by v.piloto.id
+           """)
+    float[][] getMediaHorasVooByCompanhiaAereaId(@PathVariable long companhiaAereaId);
 }
